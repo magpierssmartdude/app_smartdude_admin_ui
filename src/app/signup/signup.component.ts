18b87da6449c3b83b51678fe95ba6a060f531f9d
amrishkhan05 <amrishkhan05@gmail.com>
2019-06-18 22:55:02 +0530

    responsiveness

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -29,28 +29,30 @@ export class SignupComponent implements OnInit, OnDestroy {
       vendorName: ['', Validators.required],
       vendorCode: ['', Validators.required],
       OrgName: ['', Validators.required],
-      email: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern('[^ @]*@[^ @]*')]],
       phoneNumber: ['', Validators.required],
       OrgType: ['', Validators.required],
     });
   }
   signUp() {
+    console.log(this.signUpModel.signupForm.valid);
     this.signUpModel.signUpClicked = true;
     this.checkFormFields();
-    if(this.signUpModel.checkboxValue) {
-    const signUpDto = this.createSignUpDto(this.signUpModel.signupForm.value);
-    this.vendorSignUpService.setSignUpDetaills(signUpDto)
-      .subscribe((data => {
-        console.log(data);
-        this.toastMessage.clear();
-        this.toastMessage.add({
-          severity: 'success', summary: 'Sign Up Successful',
-          detail: 'You have signed up successfully.'
-        });
-      }));
-  }}
+    if (this.signUpModel.checkboxValue && this.signUpModel.signupForm.valid) {
+      const signUpDto = this.createSignUpDto(this.signUpModel.signupForm.value);
+      this.vendorSignUpService.setSignUpDetaills(signUpDto)
+        .subscribe((data => {
+          console.log(data);
+          this.toastMessage.clear();
+          this.toastMessage.add({
+            severity: 'success', summary: 'Sign Up Successful',
+            detail: 'You have signed up successfully.'
+          });
+        }));
+    }
+  }
   createSignUpDto(formValue) {
-   return {
+    return {
       vendorcode: formValue.vendorName,
       vendorname: formValue.vendorCode,
       organizationtype: formValue.OrgType,
@@ -59,11 +61,34 @@ export class SignupComponent implements OnInit, OnDestroy {
       status: 0
     };
   }
-  checkFormFields(){
-    
+  validateVendorName(usertyped: string, formControlName: string) {
+    const userEntry = usertyped.trim();
+    if (formControlName === 'email') {
+      this.validateEmail(usertyped);
+    } else {
+      if (utils.isEmpty(userEntry)) {
+        this.signUpModel.signupForm.controls[formControlName].setErrors({ 'fieldMandatory': 'provide a value' });
+      }
+    }
+  }
+  validateEmail(usertyped: string) {
+    const status = this.signUpModel.signupForm.controls.email.status;
+    const value = this.signUpModel.signupForm.controls.email.value.trim();
+    if (status === 'INVALID' && utils.isEmpty(value)) {
+      this.signUpModel.signupForm.controls['email'].setErrors({ 'fieldMandatory': 'provide an email' });
+    } else if (status === 'INVALID' && !utils.isEmpty(value)) {
+      this.signUpModel.signupForm.controls['email'].setErrors({ 'inValidMail': 'Please provide a valid email' });
+    }
+  }
+  checkFormFields() {
+    utils.forIn(this.signUpModel.signupForm.controls, (value: any, name: string, object: any) => {
+      this.signUpModel.signupForm.controls[name].markAsTouched();
+      if (utils.isEmpty(this.signUpModel.signupForm.controls[name].value)) {
+        this.signUpModel.signupForm.controls[name].setErrors({ 'fieldMandatory': 'Select an Equipment Type' });
+      }
+    });
   }
   checkBoxValue() {
     this.signUpModel.checkboxValue = !this.signUpModel.checkboxValue;
-    console.log(this.signUpModel.checkboxValue)
   }
 }
