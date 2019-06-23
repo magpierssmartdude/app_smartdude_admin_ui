c5f1928dc6027df3c1642208e56f1ee52732f6eb
ak <ak@gmail.com>
2019-06-23 13:44:12 +0530

    css changes

--- src/app/sign-up-requests/sign-up-requests.module.ts
+++ src/app/sign-up-requests/sign-up-requests.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignUpRequestsComponent } from './sign-up-requests.component';
-
+import { ButtonModule } from 'primeng/button';
 @NgModule({
   declarations: [SignUpRequestsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonModule
   ]
 })
 export class SignUpRequestsModule { }
